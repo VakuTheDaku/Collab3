f3bfb0b87d1dc93cc4c02e4b26193a0a83765d2e
unniznd <aanandunnis@gmail.com>
2023-12-10 03:32:50 +0530

    fixes in backend

--- backend/audio_process/urls.py
+++ backend/audio_process/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('split_background/', SplitBackground.as_view(), name='splitBackground'),
-    path('process/', ProcessAudioAPI.as_view(), name="process-audio")
+    path('process/', ProcessAudioAPI.as_view(), name="process-audio"),
+    path('overlay/', OverlayAudio.as_view(), name="overlay-audio")
 ]
